e703953d443e732cf2fe2cdbbf43de3612048e62
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-07 08:29:02 -0500

    [tdd][02] Setup config, folders, and files

---
+++ js/tdd/02/test/player.test.js
@@ -0,0 +1,17 @@
+const Player = require('../src/player');
+
+describe('Player', () => {
+  describe('General', () => {
+    it('should be constructable', () => {
+      expect(new Player()).toBeInstanceOf(Player);
+    });  
+  });
+
+  describe('Instance properties', () => {
+
+  });
+
+  describe('Instance methods', () => {
+
+  });
+});
